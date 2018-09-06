df3eb41f681b9425fedc2e9b2687f0d75d3a8ee2
Rana Emad <rana3emad@gmail.com>
2018-09-06 18:44:30 +0200

    Set view engine

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var app = require("express")();
 
+app.set("view engine","ejs");
+app.set("views","views");
 //run server
 app.listen(process.env.PORT || 3000, function () {
   console.log('Chat running ...');
