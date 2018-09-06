5c0e5a36584270444c0327b0ef3eed9839598396
Rana Emad <rana3emad@gmail.com>
2018-09-06 18:50:06 +0200

    Home page api

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = require("express")();
-
+//set view engine
 app.set("view engine","ejs");
 app.set("views","views");
+
+//home page
+app.get("/",function(req, res){
+  res.render("chat.ejs");
+});
 //run server
 app.listen(process.env.PORT || 3000, function () {
   console.log('Chat running ...');
