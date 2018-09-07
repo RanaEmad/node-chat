7a66ed352d4a676c3b08ed7facc433c7497c1f83
Rana Emad <rana3emad@gmail.com>
2018-09-07 20:16:15 +0200

    Attach socket.io to server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var app = require("express")();
+var io = require('socket.io')();
 //set view engine
 app.set("view engine","ejs");
 app.set("views","views");
@@ -8,6 +9,8 @@ app.get("/",function(req, res){
   res.render("chat.ejs");
 });
 //run server
-app.listen(process.env.PORT || 3000, function () {
+var server=app.listen(process.env.PORT || 3000, function () {
   console.log('Chat running ...');
 });
+
+io.attach(server);
