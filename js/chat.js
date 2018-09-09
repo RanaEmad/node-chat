4617c81dd3205d5ba373780da63fa19ada6ea325
Rana Emad <rana3emad@gmail.com>
2018-09-09 21:19:51 +0200

    Socket.io client side emit on submit

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+  var socket = io();
+  socket.on('connect', function() {
+
+    $("#chatForm").submit(function(e) {
+      e.preventDefault();
+      socket.emit('sendMsg',{
+        msg: $("#msg").val(),
+      });
+      $("#msg").val("");
+      $("#msg").focus();
+    }); //chatform event
+    
+}); //socket
+
 });
